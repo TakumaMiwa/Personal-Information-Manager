cf55cc4f9e3fd89008b490941a3d501b3dfa396a
TakumaMiwa <takuma.code@gmail.com>
2021-02-20 13:45:50 +0900

    command_I/v0

--- personal_information_manager.c
+++ personal_information_manager.c
@@ -54,8 +54,9 @@ int main(){
     printf("Program runs\n\n");
     printf("This program stores personal information which consists of ID, Name, Birthday, Adress, and Comment.\n");
     printf("Command:\n%%Q: quit the program\n");
-    printf("%%C: display the number of data\n");
-    printf("%%P n: Print n data on the console");
+    printf("%%I: Input data from console\n");
+    printf("%%C: Display the number of data\n");
+    printf("%%P n: Print n data on the console\n");
     printf("%%R file: Input data from file\n");
     printf("%%W file: Output data to file\n");
     printf("%%F word: Find the data which includes the word\n");
@@ -63,6 +64,7 @@ int main(){
     printf("%%D ID: Delete the data which includes the ID\n");
     printf("%%E ID: Edit the data which includes the ID\n\n");
     printf("Input data one by one\n");
+
     while (get_line(line, stdin)) {
     parse_line(line);
     }
